dd0089e7f3e4426b29a5597d70cb3d76e1b23af9
strcpy <381876024@qq.com>
2014-10-16 23:00:37 +0800

    Revert "Revert "10.16的更新~""
    This reverts commit cb51416d74667eaf18853a3a1d997c09eb73d36d.

---
+++ 第2章 C语言概述/例题/例2.3 two_func.cpp
@@ -0,0 +1,24 @@
+/*
+name:Strcpy
+date:2014/10/16
+function:在一个文件中使用两个函数
+*/
+
+#include<stdio.h>
+
+void butler(); //声明函数，通知编译器要用到的函数.
+
+int main(void)
+{
+	printf("I will summon the butler"); // words：  summon(vt. 传唤)  butler(n. 男管家）
+	butler();
+	printf("Yes,Bring me some tea and writeable CD-ROMS.\n");
+
+	return 0;
+}
+
+
+void butler()
+{
+	printf("You rang,sir?\n");
+}
