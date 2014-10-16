7cbff4ceb45bc0f78d01a5f16f470e9c0f4d57ab
strcpy <381876024@qq.com>
2014-10-16 22:39:13 +0800

    10.16的更新~
    加上了
    */
    name:
    date:
    function:
    */
    的注释，感觉高大上了很多。
    今天还测了一下几个基本的数据类型。vc6.0有几个还测不了..

---
+++ 第3章 数据和C/例题/sizeof一些基本的数据类型.cpp
@@ -0,0 +1,26 @@
+/*
+name:Strcpy
+date:2014/10/16
+function:测试数据类型的所占用空间的大小 64位win7
+*/
+
+#include<stdio.h>
+int main(void)
+{
+	printf("int:%d\n", sizeof(int));                             //int
+	printf("short int:%d\n", sizeof(short int));                 //short int
+	printf("long int:%d\n", sizeof(long int));                   //long int
+	printf("long long int:%d\n", sizeof(long long int));         //long long int
+	printf("unsigned int:%d\n", sizeof(unsigned int));           //unsigned int
+	printf("unsigned long int:%d\n", sizeof(unsigned long int)); //unsigned long int 
+	printf("float:%d\n", sizeof(float));                         //float
+	printf("double:%d\n", sizeof(double));                       //double
+	printf("long double:%d\n", sizeof(long double));             //long double
+	printf("char:%d\n", sizeof(char));                           //char
+	printf("bool:%d\n", sizeof(bool));                           //bool
+
+	getchar();
+
+		return 0;
+}
+
